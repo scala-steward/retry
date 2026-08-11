62b0af4e9fbe4c8befa5aa709fa4c8690350e4b4
Mareks Rampāns <8796159+mr-git@users.noreply.github.com>
2026-08-11 09:56:18 +0300

    Update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,8 +2,8 @@ import sbt._
 
 object Dependencies {
 
-  val scalatest     = "org.scalatest"       %% "scalatest"   % "3.2.20"
+  val scalatest = "org.scalatest" %% "scalatest" % "3.2.20"
   val `cats-helper` = "com.evolutiongaming" %% "cats-helper" % "3.12.2"
-  val random        = "com.evolution"       %% "random"      % "1.0.5"
-  val `cats-effect` = "org.typelevel"       %% "cats-effect" % "3.7.0"
+  val random = "com.evolution" %% "random" % "1.0.5"
+  val `cats-effect` = "org.typelevel" %% "cats-effect" % "3.7.0"
 }
